34a5838e6aa7d0c2bab48a60f0432b4c7a900323
JeD <jedartois@gmail.com>
2013-07-09 14:26:40 +0200

    add server and model

---
+++ org.daum.planrouge.model/src/main/java/org/daum/planrouge/model/Agent.java
@@ -0,0 +1,11 @@
+package org.daum.planrouge.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jed
+ * Date: 09/07/13
+ * Time: 13:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class Agent {
+}
