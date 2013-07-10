fecc28b55129283d94adbb8ba07c4229b57b3ba7
JeD <jedartois@gmail.com>
2013-07-10 11:17:29 +0200

    example create model

---
+++ org.daum.planrouge.server/src/main/java/org/daum/planrouge/server/DemoModel.java
@@ -0,0 +1,39 @@
+package org.daum.planrouge.server;
+
+import org.kevoree.planrouge.ContainerRoot;
+import org.kevoree.planrouge.Intervention;
+import org.kevoree.planrouge.PlanrougeFactory;
+import org.kevoree.planrouge.Victime;
+import org.kevoree.planrouge.factory.MainFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jed
+ * Date: 10/07/13
+ * Time: 11:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class DemoModel {
+
+
+    public static  void main(String argv[]) {
+        PlanrougeFactory f = new MainFactory().getPlanrougeFactory();
+
+        ContainerRoot containerRoot =f.createContainerRoot();
+
+        Intervention intervention =  f.createIntervention();
+        intervention.setId("393939");
+        intervention.setDescription("un train est rentré dans un avion en plein vol");
+
+        Victime victime =  f.createVictime();
+        victime.setId("tagid");
+        victime.setNom("briand");
+        victime.setPrenom("clement");
+
+        intervention.addVictimes(victime);
+
+
+        containerRoot.addInterventions(intervention);
+
+    }
+}
